e005322d5ffe8c17265216e7e21faf76ad150782
Potter <75838259@qq.com>
2022-03-15 14:45:43 +0800

    init project：vendor.js? [sm]:3545 TypeError: Cannot read property 'fn' of undefined

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<Sub>
+		<template #first="{ item }">
+			<view class="item"> xxx {{ item.name }} </view>
+		</template>
+	</Sub>
+</template>
+
+<script>
+export default {
+	onLaunch: function() {
+		console.log("App Launch");
+	},
+	onShow: function() {
+		console.log("App Show");
+	},
+	onHide: function() {
+		console.log("App Hide");
+	},
+};
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		test page
+		<Sub>
+			<template #first="{ item }">
+				<view class="item"> xxx {{ item.name }} </view>
+			</template>
+		</Sub>
+	</view>
+</template>
+
+<script>
+import Sub from "./sub.vue";
+
+export default {
+	components: {
+		Sub,
+	},
+	onLoad() {},
+};
+</script>
