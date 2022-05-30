fe6e3fca219bac6fbaf3dc6316f72634698ab735
Potter <75838259@qq.com>
2022-05-30 21:18:29 +0800

    补充：漏上传图标

---
+++ src/pages/cart/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>
+    <button type="default" @tap="jump">跳转到测试页面</button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {
+    jump() {
+      console.log("jump:跳转---------------");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/pages/category/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 设计我家 -->
+  <view>
+    <button type="default" @tap="jump">跳转到测试页面</button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {
+    jump() {
+      console.log("jump:跳转---------------");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
